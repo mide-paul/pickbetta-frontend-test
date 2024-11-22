fe7186e73d9fda2da73e5626e8e5837d7d32e4d1
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2024-11-22 07:14:08 +0100

    Update on Login screen for laptop

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -109,12 +109,12 @@ export const Login = () => {
   }
 
   return (
-    <div className="ss:h-46 ss:w-full ss:bg-gray-white ss:overflow-hidden   sm:h-45 sm:w-full sm:bg-gray-white sm:overflow-hidden   mm:h-45 mm:w-full mm:bg-gray-white mm:overflow-hidden    md:h-45 md:w-full md:bg-gray-white md:overflow-hidden    ">
+    <div className="ss:h-46 ss:w-full ss:bg-gray-white ss:overflow-hidden   sm:h-45 sm:w-full sm:bg-gray-white sm:overflow-hidden   mm:h-45 mm:w-full mm:bg-gray-white mm:overflow-hidden    md:h-45 md:w-full md:bg-gray-white md:overflow-hidden     x:h-45 x:w-full x:bg-gray-white x:overflow-hidden    ">
       <div>
         <img
           src={logo}
           alt=""
-          className="absolute  ss:mt-3 ss:ml-13 ss:z-10    sm:mt-3 sm:ml-13 sm:z-10   mm:mt-3 mm:ml-13 mm:z-10    md:mt-3 md:ml-15 md:z-10  "
+          className="absolute  ss:mt-3 ss:ml-13 ss:z-10    sm:mt-3 sm:ml-13 sm:z-10   mm:mt-3 mm:ml-13 mm:z-10    md:mt-3 md:ml-15 md:z-10     x:mt-10 x:ml-28 x:z-10 "
         />
       </div>
 
@@ -123,10 +123,10 @@ export const Login = () => {
           <img
             src={arrow}
             alt=""
-            className="relative  ss:mt-9 ss:ml-4 ss:w-4     sm:mt-9 sm:ml-4 sm:w-4   mm:mt-9 mm:ml-4 mm:w-4  md:mt-9 md:ml-15 md: w-4  "
+            className="relative  ss:mt-9 ss:ml-4 ss:w-4     sm:mt-9 sm:ml-4 sm:w-4   mm:mt-9 mm:ml-4 mm:w-4  md:mt-9 md:ml-15 md:w-4     x:mt-13 x:ml-28 x:w-4 "
           />
         </Link>
-        <h3 className="relative  ss:ml-4 ss:mt-4  ss:text-dark-gray-900 ss:text-left ss:text-base      sm:ml-4 sm:mt-4  sm:text-dark-gray-900 sm:text-left sm:text-base      mm:ml-4 mm:mt-4  mm:text-dark-gray-900 mm:text-left mm:text-base   md:ml-15 md:mt-4  md:text-dark-gray-900 md:text-left md:text-base   font-medium font-montserrat">
+        <h3 className="relative  ss:ml-4 ss:mt-4  ss:text-dark-gray-900 ss:text-left ss:text-base      sm:ml-4 sm:mt-4  sm:text-dark-gray-900 sm:text-left sm:text-base      mm:ml-4 mm:mt-4  mm:text-dark-gray-900 mm:text-left mm:text-base   md:ml-15 md:mt-4  md:text-dark-gray-900 md:text-left md:text-base   x:ml-28 x:mt-4  x:text-dark-gray-900 x:text-left x:text-base  font-medium font-montserrat">
           Welcome Back,
         </h3>
       </div>
@@ -135,7 +135,7 @@ export const Login = () => {
         <div className="flex flex-col">
           <label
             htmlFor="email"
-            className="relative  ss:ml-4 ss:mt-4 ss:text-base  ss:text-left      sm:ml-4 sm:mt-4 sm:text-base  sm:text-left   mm:ml-4 mm:mt-4 mm:text-base  mm:text-left      md:ml-15 md:mt-4 md:text-base  md:text-left   text-gray  font-montserrat"
+            className="relative  ss:ml-4 ss:mt-4 ss:text-base  ss:text-left      sm:ml-4 sm:mt-4 sm:text-base  sm:text-left   mm:ml-4 mm:mt-4 mm:text-base  mm:text-left      md:ml-15 md:mt-4 md:text-base  md:text-left   x:ml-28 x:mt-4 x:text-base  x:text-left    text-gray  font-montserrat"
           >
             Email
             <FontAwesomeIcon
@@ -159,7 +159,7 @@ export const Login = () => {
             aria-describedby="uidnote"
             onFocus={() => setEmailFocus(true)}
             onBlur={() => setEmailFocus(false)}
-            className="relative  ss:ml-4 ss:mt-2 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3     sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-22 sm:text-base sm:pl-3 sm:pr-3     mm:ml-4 mm:mt-2 mm:h-6.4 mm:w-22 mm:text-base mm:pl-3 mm:pr-3   md:ml-15 md:mt-2 md:h-6.4 md:w-25 md:text-base md:pl-3 md:pr-3         border rounded font-montserrat"
+            className="relative  ss:ml-4 ss:mt-2 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3     sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-22 sm:text-base sm:pl-3 sm:pr-3     mm:ml-4 mm:mt-2 mm:h-6.4 mm:w-22 mm:text-base mm:pl-3 mm:pr-3   md:ml-15 md:mt-2 md:h-6.4 md:w-25 md:text-base md:pl-3 md:pr-3  x:ml-28 x:mt-2 x:h-6.4 x:w-25 x:text-base x:pl-3 x:pr-3       border rounded font-montserrat"
           />
 
           <p
@@ -179,7 +179,7 @@ export const Login = () => {
         <div className="flex flex-col">
           <label
             htmlFor="password"
-            className="relative  ss:ml-4 ss:mt-4 ss:text-base  ss:text-left     sm:ml-4 sm:mt-4 sm:text-base  sm:text-left   mm:ml-4 mm:mt-4 mm:text-base  mm:text-left      md:ml-15 md:mt-4 md:text-base  md:text-left      text-gray  font-montserrat"
+            className="relative  ss:ml-4 ss:mt-4 ss:text-base  ss:text-left     sm:ml-4 sm:mt-4 sm:text-base  sm:text-left   mm:ml-4 mm:mt-4 mm:text-base  mm:text-left      md:ml-15 md:mt-4 md:text-base  md:text-left   x:ml-28 x:mt-4 x:text-base  x:text-left      text-gray  font-montserrat"
           >
             Password
             <FontAwesomeIcon
@@ -203,7 +203,7 @@ export const Login = () => {
             aria-describedby="pwdnote"
             onFocus={() => setPwdFocus(true)}
             onBlur={() => setPwdFocus(false)}
-            className="relative  ss:ml-4 ss:mt-2 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-6.2   sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-22 sm:text-base sm:pl-3 sm:pr-6.2   mm:ml-4 mm:mt-2 mm:h-6.4 mm:w-22 mm:text-base mm:pl-3 mm:pr-6.2   md:ml-15 md:mt-2 md:h-6.4 md:w-25 md:text-base md:pl-3 md:pr-6.2     border rounded font-montserrat"
+            className="relative  ss:ml-4 ss:mt-2 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-6.2   sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-22 sm:text-base sm:pl-3 sm:pr-6.2   mm:ml-4 mm:mt-2 mm:h-6.4 mm:w-22 mm:text-base mm:pl-3 mm:pr-6.2   md:ml-15 md:mt-2 md:h-6.4 md:w-25 md:text-base md:pl-3 md:pr-6.2   x:ml-28 x:mt-2 x:h-6.4 x:w-25 x:text-base x:pl-3 x:pr-6.2     border rounded font-montserrat"
           />
 
           <p
@@ -226,7 +226,7 @@ export const Login = () => {
 
           <span className="items-center" onClick={handleToggle}>
             <Icon
-              className="absolute  ss:ml-12 ss:-mt-6.1 ss:z-10 ss:cursor-pointer  ss:text-gray    sm:ml-12 sm:-mt-6.1 sm:z-10 sm:cursor-pointer  sm:text-gray   mm:ml-12 mm:-mt-6.1 mm:z-10 mm:cursor-pointer  mm:text-gray     md:ml-13 md:-mt-6.1 md:z-10 md:cursor-pointer  md:text-gray  "
+              className="absolute  ss:ml-12 ss:-mt-6.1 ss:z-10 ss:cursor-pointer  ss:text-gray    sm:ml-12 sm:-mt-6.1 sm:z-10 sm:cursor-pointer  sm:text-gray   mm:ml-12 mm:-mt-6.1 mm:z-10 mm:cursor-pointer  mm:text-gray     md:ml-13 md:-mt-6.1 md:z-10 md:cursor-pointer  md:text-gray      x:ml-13 x:-mt-6.1 x:z-10 x:cursor-pointer  x:text-gray  "
               icon={icon}
               size={20}
             />
@@ -236,12 +236,12 @@ export const Login = () => {
         <div>
           <input
             type="radio"
-            className="relative   ss:ml-4 ss:mt-4     sm:ml-4 sm:mt-4    mm:ml-4 mm:mt-4   md:ml-15 md:mt-4  text-gray  float-left"
+            className="relative   ss:ml-4 ss:mt-4     sm:ml-4 sm:mt-4    mm:ml-4 mm:mt-4   md:ml-15 md:mt-4   x:ml-28 x:mt-4 text-gray  float-left"
           />
-          <h3 className="absolute   ss:ml-6.2 ss:mt-4 ss:text-sm  ss:text-left       sm:ml-6.2 sm:mt-4 sm:text-sm  sm:text-left    mm:ml-6.2 mm:mt-4 mm:text-sm  mm:text-left     md:ml-15.6 md:mt-4 md:text-sm  md:text-left    text-dark font-montserrat">
+          <h3 className="absolute   ss:ml-6.2 ss:mt-4 ss:text-sm  ss:text-left       sm:ml-6.2 sm:mt-4 sm:text-sm  sm:text-left    mm:ml-6.2 mm:mt-4 mm:text-sm  mm:text-left     md:ml-15.6 md:mt-4 md:text-sm  md:text-left     x:ml-29 x:mt-4 x:text-sm  x:text-left     text-dark font-montserrat">
             Remember me
           </h3>
-          <h3 className="absolute   ss:ml-17 ss:mt-4 ss:font-semibold ss:text-sm ss:text-green ss:cursor-pointer ss:underline      sm:ml-19 sm:mt-4 sm:font-semibold sm:text-sm sm:text-green sm:cursor-pointer sm:underline    mm:ml-19 mm:mt-4 mm:font-semibold mm:text-sm mm:text-green mm:cursor-pointer mm:underline     md:ml-27 md:mt-4 md:font-semibold md:text-sm md:text-green md:cursor-pointer md:underline  font-montserrat">
+          <h3 className="absolute   ss:ml-17 ss:mt-4 ss:font-semibold ss:text-sm ss:text-green ss:cursor-pointer ss:underline      sm:ml-19 sm:mt-4 sm:font-semibold sm:text-sm sm:text-green sm:cursor-pointer sm:underline    mm:ml-19 mm:mt-4 mm:font-semibold mm:text-sm mm:text-green mm:cursor-pointer mm:underline     md:ml-27 md:mt-4 md:font-semibold md:text-sm md:text-green md:cursor-pointer md:underline     x:ml-45 x:mt-4 x:font-semibold x:text-sm x:text-green x:cursor-pointer x:underline    font-montserrat">
             Forgot Password?
           </h3>
         </div>
@@ -249,7 +249,7 @@ export const Login = () => {
         <button
           type="button"
           onClick={userLogin}
-          className="relative   ss:mt-8 ss:-ml-1 ss:h-6.4 ss:w-19.4 ss:bg-green ss:text-white ss:text-base ss:rounded ss:disabled:bg-gray ss:disabled:text-white     sm:mt-8 sm:-ml-6.5 sm:h-6.4 sm:w-21.3 sm:bg-green sm:text-white sm:text-base sm:rounded sm:disabled:bg-gray sm:disabled:text-white   mm:mt-8 mm:-ml-8 mm:h-6.4 mm:w-21.3 mm:bg-green mm:text-white mm:text-base mm:rounded mm:disabled:bg-gray mm:disabled:text-white   md:-ml-17 md:h-6.4 md:w-25 md:bg-green md:text-white md:text-base md:rounded md:disabled:bg-gray md:disabled:text-white      font-montserrat"
+          className="relative   ss:mt-8 ss:-ml-1 ss:h-6.4 ss:w-19.4 ss:bg-green ss:text-white ss:text-base ss:rounded ss:disabled:bg-gray ss:disabled:text-white     sm:mt-8 sm:-ml-6.5 sm:h-6.4 sm:w-21.3 sm:bg-green sm:text-white sm:text-base sm:rounded sm:disabled:bg-gray sm:disabled:text-white   mm:mt-8 mm:-ml-8 mm:h-6.4 mm:w-21.3 mm:bg-green mm:text-white mm:text-base mm:rounded mm:disabled:bg-gray mm:disabled:text-white   md:-ml-17 md:h-6.4 md:w-25 md:bg-green md:text-white md:text-base md:rounded md:disabled:bg-gray md:disabled:text-white    x:-ml-32 x:h-6.4 x:w-25 x:bg-green x:text-white x:text-base x:rounded x:disabled:bg-gray x:disabled:text-white       font-montserrat"
           disabled={login == null}
         >
           Login
@@ -260,20 +260,20 @@ export const Login = () => {
         <img
           src={line}
           alt=""
-          className="absolute   ss:mt-2 ss:ml-4 ss:h-0.1 ss:w-11     sm:mt-2 sm:ml-4 sm:h-0.1 sm:w-12.1  mm:mt-2 mm:ml-4 mm:h-0.1 mm:w-13   md:mt-2 md:ml-15 md:h-0.1 md:w-13   "
+          className="absolute   ss:mt-2 ss:ml-4 ss:h-0.1 ss:w-11     sm:mt-2 sm:ml-4 sm:h-0.1 sm:w-12.1  mm:mt-2 mm:ml-4 mm:h-0.1 mm:w-13   md:mt-2 md:ml-15 md:h-0.1 md:w-13      x:mt-2 x:ml-28 x:h-0.1 x:w-13  "
         />
-        <h3 className="relative  ss:mt-5  ss:ml-4 ss:text-sm  ss:text-gray       sm:mt-5  sm:text-sm  sm:text-gray    mm:mt-5  mm:text-sm  mm:text-gray mm:-ml-4     md:mt-5  md:text-sm  md:text-gray md:-ml-8   font-montserrat">
+        <h3 className="relative  ss:mt-5  ss:ml-4 ss:text-sm  ss:text-gray       sm:mt-5  sm:text-sm  sm:text-gray    mm:mt-5  mm:text-sm  mm:text-gray mm:-ml-4     md:mt-5  md:text-sm  md:text-gray md:-ml-8    x:mt-5  x:text-sm  x:text-gray x:-ml-8   font-montserrat">
           or, login using
         </h3>
         <img
           src={line}
           alt=""
-          className="absolute    ss:-mt-1 ss:ml-17 ss:h-0.1 ss:w-11.9       sm:-mt-1 sm:ml-17.7 sm:h-0.1 sm:w-11.9   mm:-mt-1 mm:ml-19.1 mm:h-0.1 mm:w-11.9     md:-mt-1 md:ml-25 md:h-0.1 md:w-15    "
+          className="absolute    ss:-mt-1 ss:ml-17 ss:h-0.1 ss:w-11.9       sm:-mt-1 sm:ml-17.7 sm:h-0.1 sm:w-11.9   mm:-mt-1 mm:ml-19.1 mm:h-0.1 mm:w-11.9     md:-mt-1 md:ml-25 md:h-0.1 md:w-15    x:-mt-1 x:ml-43 x:h-0.1 x:w-15 "
         />
       </div>
 
       <div className="flex gap-1">
-        <div className=" ss:h-6.4 ss:w-11 ss:mt-5 ss:ml-4 ss:bg-white ss:border  ss:border-gray  ss:rounded  sm:border-gray       sm:h-6.4 sm:w-11 sm:mt-5 sm:ml-4 sm:bg-white sm:border sm:rounded   mm:h-6.4 mm:w-11 mm:mt-5 mm:ml-4 mm:bg-white mm:border mm:rounded mm:border-gray      md:h-6.4 md:w-11 md:mt-5 md:ml-15 md:bg-white md:border md:border-gray md:rounded ">
+        <div className=" ss:h-6.4 ss:w-11 ss:mt-5 ss:ml-4 ss:bg-white ss:border  ss:border-gray  ss:rounded  sm:border-gray       sm:h-6.4 sm:w-11 sm:mt-5 sm:ml-4 sm:bg-white sm:border sm:rounded   mm:h-6.4 mm:w-11 mm:mt-5 mm:ml-4 mm:bg-white mm:border mm:rounded mm:border-gray      md:h-6.4 md:w-11 md:mt-5 md:ml-15 md:bg-white md:border md:border-gray md:rounded    x:h-6.4 x:w-11 x:mt-5 x:ml-28 x:bg-white x:border x:border-gray x:rounded ">
           <img
             src={google}
             alt=""
