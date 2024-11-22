e9eb1e5c04601b1d50c62dbf567b04650d48be49
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2024-11-22 09:02:20 +0100

    Update on small screen and medium screen for sign up

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -151,21 +151,48 @@ export const Signup = () => {
   return (
     <div className="sm:h-60 w-full bg-gray-white overflow-hidden">
       <div>
-        <img src={logo} alt="" className="absolute mt-3 ml-13 z-10" />
+        <img
+          src={logo}
+          alt=""
+          className="absolute ss:mt-3 ss:ml-2 ss:z-10 sm:mt-3 sm:ml-13 sm:z-10"
+        />
       </div>
 
       <div>
-        <Link to="/"><img src={arrow} alt="" className="relative mt-9 ml-4 w-4" /></Link>
-        <h3 className='relative ml-4 mt-4 font-medium text-dark text-left text-base font-montserrat'>Create Account</h3>
+        <Link to="/">
+          <img
+            src={arrow}
+            alt=""
+            className="relative ss:mt-9 ss:ml-4 ss:w-4  sm:mt-9 sm:ml-4 sm:w-4  "
+          />
+        </Link>
+        <h3 className="relative ss:ml-4 ss:mt-4 ss:font-medium ss:text-dark ss:text-left ss:text-base  sm:ml-4 sm:mt-4 sm:font-medium sm:text-dark sm:text-left sm:text-base font-montserrat">
+          Create Account
+        </h3>
       </div>
 
-      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
       <form onSubmit={handleSubmit}>
-        <div className='flex flex-col'>
-          <label htmlFor="firstname" className='relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat'>
+        <div className="flex flex-col">
+          <label
+            htmlFor="firstname"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Firstname
-            <FontAwesomeIcon icon={faCheck} className={firstnameIsValid ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={firstnameIsValid || !firstname ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={firstnameIsValid ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={firstnameIsValid || !firstname ? "hide" : "invalid"}
+            />
           </label>
           <input
             type="text"
@@ -179,22 +206,40 @@ export const Signup = () => {
             aria-describedby="uidnote"
             onFocus={() => setUserFocus(true)}
             onBlur={() => setUserFocus(false)}
-            className='relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base pl-3 pr-3 border rounded font-montserrat'
+            className="relative ss:ml-4 ss:mt-2 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
 
-          <p id="uidnote" className={userFocus && firstname && !firstnameIsValid ? "instructions" : "offscreen"}>
+          <p
+            id="uidnote"
+            className={
+              userFocus && firstname && !firstnameIsValid
+                ? "instructions"
+                : "offscreen"
+            }
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
-            4 to 30 characters.<br />
-            Must begin with a letter.<br />
+            4 to 30 characters.
+            <br />
+            Must begin with a letter.
+            <br />
             Letters, numbers, underscores, hyphens allowed.
           </p>
         </div>
 
-        <div className='flex flex-col'>
-          <label htmlFor="lastname" className='relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat'>
+        <div className="flex flex-col">
+          <label
+            htmlFor="lastname"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Lastname
-            <FontAwesomeIcon icon={faCheck} className={lastnameIsValid ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={lastnameIsValid || !lastname ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={lastnameIsValid ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={lastnameIsValid || !lastname ? "hide" : "invalid"}
+            />
           </label>
           <input
             type="text"
@@ -207,22 +252,40 @@ export const Signup = () => {
             aria-describedby="uidnote"
             onFocus={() => setUsernameFocus(true)}
             onBlur={() => setUsernameFocus(false)}
-            className='relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base pl-3 pr-3 border rounded font-montserrat'
+            className="relative ss:mt-2  ss:ml-4 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
 
-          <p id="uidnote" className={usernameFocus && lastname && !lastnameIsValid ? "instructions" : "offscreen"}>
+          <p
+            id="uidnote"
+            className={
+              usernameFocus && lastname && !lastnameIsValid
+                ? "instructions"
+                : "offscreen"
+            }
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
-            4 to 30 characters.<br />
-            Must begin with a letter.<br />
+            4 to 30 characters.
+            <br />
+            Must begin with a letter.
+            <br />
             Letters, numbers, underscores, hyphens allowed.
           </p>
         </div>
 
-        <div className='flex flex-col'>
-          <label htmlFor="email" className='relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat'>
+        <div className="flex flex-col">
+          <label
+            htmlFor="email"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Email
-            <FontAwesomeIcon icon={faCheck} className={validEmail ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={validEmail || !email ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validEmail ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validEmail || !email ? "hide" : "invalid"}
+            />
           </label>
           <input
             type="email"
@@ -236,24 +299,37 @@ export const Signup = () => {
             aria-describedby="uidnote"
             onFocus={() => setEmailFocus(true)}
             onBlur={() => setEmailFocus(false)}
-            className='relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base pl-3 pr-3 border rounded font-montserrat'
+            className="relative ss:ml-4 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
 
-          <p id="uidnote" className={emailFocus && email &&
-            !validEmail ? "instructions" : "offscreen"}>
+          <p
+            id="uidnote"
+            className={
+              emailFocus && email && !validEmail ? "instructions" : "offscreen"
+            }
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
-            4 to 40 characters.<br />
+            4 to 40 characters.
+            <br />
             Must begin with a letter. <br />
-            only lowercase is allowed.
-            Allowed special characters: @
+            only lowercase is allowed. Allowed special characters: @
           </p>
         </div>
 
-        <div className='flex flex-col'>
-          <label htmlFor="phonenumber" className='relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat'>
+        <div className="flex flex-col">
+          <label
+            htmlFor="phonenumber"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Phone Number
-            <FontAwesomeIcon icon={faCheck} className={validNumber ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={validNumber || !number ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validNumber ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validNumber || !number ? "hide" : "invalid"}
+            />
           </label>
           <input
             type="number"
@@ -267,24 +343,41 @@ export const Signup = () => {
             aria-describedby="uidnote"
             onFocus={() => setNumberFocus(true)}
             onBlur={() => setNumberFocus(false)}
-            className='relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base pl-6.2 pr-3 border rounded font-montserrat'
+            className="relative ss:ml-4 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
-          <h3 className="absolute sm:ml-5.2 sm:mt-8.6 text-sm text-dark text-left font-montserrat">US</h3>
+          <h3 className="absolute sm:ml-5.2 sm:mt-8.6 text-sm text-dark text-left font-montserrat">
+            US
+          </h3>
 
-          <p id="uidnote" className={numberFocus && number &&
-            !validNumber ? "instructions" : "offscreen"}>
+          <p
+            id="uidnote"
+            className={
+              numberFocus && number && !validNumber
+                ? "instructions"
+                : "offscreen"
+            }
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
-            10 characters.<br />
-            Must begin with a number.
-            only numbers are allowed.
+            10 characters.
+            <br />
+            Must begin with a number. only numbers are allowed.
           </p>
         </div>
 
-        <div className='flex flex-col'>
-          <label htmlFor="password" className='relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat'>
+        <div className="flex flex-col">
+          <label
+            htmlFor="password"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Password
-            <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validPwd ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validPwd || !pwd ? "hide" : "invalid"}
+            />
           </label>
           <input
             type={type}
@@ -297,26 +390,50 @@ export const Signup = () => {
             aria-describedby="pwdnote"
             onFocus={() => setPwdFocus(true)}
             onBlur={() => setPwdFocus(false)}
-            className='relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base pl-3 pr-6.2 border rounded font-montserrat'
+            className="relative ss:ml-4 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
 
-          <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+          <p
+            id="pwdnote"
+            className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
-            8 to 15 characters.<br />
-            Must include uppercase and lowercase letters, a number and a special character.<br />
-            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+            8 to 15 characters.
+            <br />
+            Must include uppercase and lowercase letters, a number and a special
+            character.
+            <br />
+            Allowed special characters:{" "}
+            <span aria-label="exclamation mark">!</span>{" "}
+            <span aria-label="at symbol">@</span>{" "}
+            <span aria-label="hashtag">#</span>{" "}
+            <span aria-label="dollar sign">$</span>{" "}
+            <span aria-label="percent">%</span>
           </p>
 
           <span className="items-center" onClick={handleToggle}>
-            <Icon className="absolute sm:ml-13 sm:-mt-6.1 z-10 cursor-pointer" icon={icon} size={20} />
+            <Icon
+              className="absolute ss:ml-12 ss:-mt-6.1 ss:z-10 ss:cursor-pointer ss:text-gray    sm:ml-13 sm:-mt-6.1 sm:z-10 sm:cursor-pointer sm:text-gray"
+              icon={icon}
+              size={20}
+            />
           </span>
         </div>
 
-        <div className='flex flex-col'>
-          <label htmlFor="confirm_pwd" className="relative sm:ml-4 sm:mt-4 sm:text-base text-gray text-left font-montserrat">
+        <div className="flex flex-col">
+          <label
+            htmlFor="confirm_pwd"
+            className="relative ss:ml-4 ss:mt-4 ss:text-base ss:text-gray ss:text-left   sm:ml-4 sm:mt-4 sm:text-base sm:text-gray sm:text-left font-montserrat"
+          >
             Confirm Password
-            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className={validMatch && matchPwd ? "valid" : "hide"}
+            />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={validMatch || !matchPwd ? "hide" : "invalid"}
+            />
           </label>
           <input
             type={type}
@@ -329,32 +446,48 @@ export const Signup = () => {
             aria-describedby="confirmnote"
             onFocus={() => setMatchFocus(true)}
             onBlur={() => setMatchFocus(false)}
-            className="relative sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 text-base pl-3 pr-6.2 border rounded font-montserrat"
+            className="relative ss:ml-4 ss:h-6.4 ss:w-19.7 ss:text-base ss:pl-3 ss:pr-3 ss:border ss:rounded ss:border-gray      sm:ml-4 sm:mt-2 sm:h-6.4 sm:w-21.3 sm:text-base sm:pl-3 sm:pr-3 sm:border sm:rounded sm:border-gray font-montserrat"
           />
-          <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+          <p
+            id="confirmnote"
+            className={matchFocus && !validMatch ? "instructions" : "offscreen"}
+          >
             <FontAwesomeIcon icon={faInfoCircle} />
             Must match the first password input field.
           </p>
           <span className="items-center" onClick={handleToggle}>
-            <Icon className="absolute sm:ml-13 sm:-mt-6.1 z-10 cursor-pointer" icon={icon} size={20} />
+            <Icon
+              className="absolute  ss:ml-12 ss:-mt-6.1 ss:z-10 ss:cursor-pointer ss:text-gray    sm:ml-13 sm:-mt-6.1 sm:z-10 sm:cursor-pointer sm:text-gray"
+              icon={icon}
+              size={20}
+            />
           </span>
         </div>
 
         <div>
           <input
-            type='radio'
-            className='relative ml-4 mt-6 float-left'
+            type="radio"
+            className="relative ml-4 mt-6 float-left"
             onClick={() => setLogin(LoginType.LOGIN)}
           />
-          <h3 className='absolute sm:ml-6.5 sm:mt-4.7 text-sm text-dark text-left sm:max-w-19.5 font-montserrat'>I agree to the
-            <span className='text-green font-semibold underline'> Terms & Conditions</span> and <span className='text-green font-semibold underline'>Privacy Policy</span></h3>
+          <h3 className="absolute  ss:ml-6.5 ss:mt-4.7 ss:text-sm ss:text-dark ss:text-left ss:max-w-19.5    sm:ml-6.5 sm:mt-4.7 sm:text-sm sm:text-dark sm:text-left sm:max-w-19.5 font-montserrat">
+            I agree to the
+            <span className="text-green font-semibold underline">
+              {" "}
+              Terms & Conditions
+            </span>{" "}
+            and{" "}
+            <span className="text-green font-semibold underline">
+              Privacy Policy
+            </span>
+          </h3>
         </div>
 
         <button
           //disabled={!firstnameIsValid || !lastnameIsValid || !validEmail || !validPwd || !validMatch ? true : false}
           type="submit"
           onClick={signUp}
-          className='relative sm:mt-9.5 sm:-ml-6.5 sm:h-6.4 sm:w-21.3 bg-green text-white sm:text-base rounded disabled:bg-gray-lighter disabled:text-white font-montserrat'
+          className="relative ss:mt-7 ss:ml-4 ss:h-6.4 ss:w-19.4 ss:bg-green ss:text-white ss:text-base ss:rounded ss:disabled:bg-gray-lighter ss:disabled:text-gray    sm:mt-9.5 sm:-ml-6.5 sm:h-6.4 sm:w-21.3 sm:bg-green sm:text-white sm:text-base sm:rounded sm:disabled:bg-gray-lighter sm:disabled:text-gray font-montserrat"
           disabled={login == null}
         >
           Create Account
@@ -363,11 +496,13 @@ export const Signup = () => {
       </form>
 
       <div>
-        <h3 className='relative mt-4 text-dark text-sm font-montserrat'>Already have an account?
-          <span className='text-green font-semibold cursor-pointer'>
-            <Link to="/login"> Login</Link></span>
+        <h3 className="relative ss:mt-4 ss:text-dark ss:text-sm     sm:mt-4 sm:text-dark sm:text-sm font-montserrat">
+          Already have an account?
+          <span className=" ss:text-green ss:font-semibold ss:cursor-pointer   sm:text-green sm:font-semibold sm:cursor-pointer">
+            <Link to="/login"> Login</Link>
+          </span>
         </h3>
       </div>
     </div>
-  )
+  );
 }
